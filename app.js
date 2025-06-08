bd3d9afc9d93d49a75154d23b7c304e772877a57
Animesh478 <animeshmohanty.am@gmail.com>
2025-06-08 10:12:56 +0530

    task 2 completed

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const heading = document.getElementById("main-heading");
-const header = document.getElementById("header");
-const basketHeading = document.getElementById("basket-heading");
-const thanks = document.getElementById("thanks");
+// const heading = document.getElementById("main-heading");
+// const header = document.getElementById("header");
+// const basketHeading = document.getElementById("basket-heading");
+// const thanks = document.getElementById("thanks");
 
-heading.textContent = "Fruit World";
-heading.style.color = "orange";
+// heading.textContent = "Fruit World";
+// heading.style.color = "orange";
 
-header.style.backgroundColor = "green";
-header.style.borderBottom = "2px solid orange";
+// header.style.backgroundColor = "green";
+// header.style.borderBottom = "2px solid orange";
 
-basketHeading.style.color = "green";
+// basketHeading.style.color = "green";
 
-thanks.innerHTML = "<p>Please visit us again</p>";
+// thanks.innerHTML = "<p>Please visit us again</p>";
+
+// Task - 2
+const fruit = document.getElementsByClassName("fruit");
+fruit[2].style.backgroundColor = "yellow";
+
+fruit[0].style.fontWeight = "bold";
+fruit[1].style.fontWeight = "bold";
+fruit[2].style.fontWeight = "bold";
+fruit[3].style.fontWeight = "bold";
