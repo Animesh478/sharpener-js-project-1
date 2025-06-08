085f144ce2d3d1eab77661b67243ad7872669761
Animesh478 <animeshmohanty.am@gmail.com>
2025-06-08 10:10:45 +0530

    first task completed

---
+++ app.js
@@ -0,0 +1,14 @@
+const heading = document.getElementById("main-heading");
+const header = document.getElementById("header");
+const basketHeading = document.getElementById("basket-heading");
+const thanks = document.getElementById("thanks");
+
+heading.textContent = "Fruit World";
+heading.style.color = "orange";
+
+header.style.backgroundColor = "green";
+header.style.borderBottom = "2px solid orange";
+
+basketHeading.style.color = "green";
+
+thanks.innerHTML = "<p>Please visit us again</p>";
